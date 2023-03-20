c4a1d64511ade6114c01d8b645c5203f70e5218d
oviya1992 <ovbaskaran28@gmail.com>
2023-03-20 04:34:00 -0700

    newprogram

--- UseAddition.java
+++ UseAddition.java
@@ -5,7 +5,7 @@ public class UseAddition {
 	public static void main(String[] args) {
 		
 		Addition a=new Addition();
-		System.out.println(a.add(10));
+		System.out.println(a.add(100));
 		System.out.println(a.add(10,20));
 		System.out.println(a.add(10,20,40));
 		
